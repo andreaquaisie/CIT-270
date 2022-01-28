53867ce64f517e79bdc2be2f6c211272adea5597
andreaquaisie <97315192+andreaquaisie@users.noreply.github.com>
2022-01-28 13:19:48 -0700

    Added data.json, loginscript.bat folders and new code to server.js

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser');
 const port = 3000;
 const app = express();
 
+app.use(bodyParser.json());
 app.get('/', (req,res)=>{
     res.send("Hello browser");
+});
+
+app.post('/login',(req,res) => {
+    console.log(JSON.stringify(req.body));
+    if (req.body.userName =="aquaisie" && req.body.password =="blah"){
+        res.send("Welcome!")
+    }
+    else{
+        res.send("Who are you?")
+    }
 });
 
 app.listen(port, ()=>{});
